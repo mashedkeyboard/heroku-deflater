c4beae0d0a4be1ba87b3958fdb20004526744053
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2017-06-15 15:51:48 +0100

    Set max_age to secrets.max_age

--- lib/heroku-deflater/railtie.rb
+++ lib/heroku-deflater/railtie.rb
@@ -16,7 +16,7 @@ module HerokuDeflater
     # The configuration block in config/application.rb overrides this.
     config.before_initialize do |app|
       cache_control = cache_control_manager(app)
-      cache_control.setup_max_age(86400)
+      cache_control.setup_max_age(Rails.application.secrets.max_age)
     end
 
     def self.cache_control_manager(app)
